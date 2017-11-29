b69136be25b250699d9d3365ddca550cf8d8cd29
MGlolenstine <mglolenstine@gmail.com>
2017-11-29 20:19:10 +0100

    + Added gradian noise(similar to Perlin noise) + Added a graph example of the noise + Finally added true transparency of shapes and images + Added noFill() and noStroke() that set color's alpha to 0 thus making them transparent
    If you find any bugs in the code or would do something differently, possibly improving performance, I'm open for ideas.

--- P5c.h
+++ P5c.h
@@ -35,7 +35,8 @@ const int CLOSE = 1;
 #include <IL/il.h>
 #include <ilut.h>
 #include <sstream>
-#include <stdlib.h>
+#include <algorithm>
+#include <random>
 
 // Functions
 
@@ -293,6 +294,7 @@ int mouseX;
 int mouseY;
 int mouseButton;
 int strokeweight = 1;
+int octave = 2;
 Color strokeCol; // NOLINT
 Color fillCol; // NOLINT
 int counterV = 0;
@@ -311,6 +313,8 @@ PImage loadPNG(char url[]);
 PImage read_png_file(char *file_name);
 
 PImage read_png_file1(char name[]);
+
+float noiseCalc(double x, double y, int nbOctave);
 
 int main() {
     ilInit();
@@ -433,11 +437,11 @@ void display() {
 }
 
 void background(int r, int g = 256, int b = 256, int a = 255) {
-    last = Color(r, g, b, a);
     if (g > 255 && b > 255) {
         g = r;
         b = r;
     }
+    last = Color(r, g, b, a);
     float tmpr = map(r, 0, 255, 0, 1);
     float tmpg = map(g, 0, 255, 0, 1);
     float tmpb = map(b, 0, 255, 0, 1);
@@ -471,6 +475,8 @@ void vertex(float x, float y, float z = 0) {
 
 void ellipse(float x, float y, float w, float h) {
     y = map(y, 0, height, height, 0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glPushMatrix();
     glTranslated(x, y, 0);
     glColor4d(fillCol.r, fillCol.g, fillCol.b, fillCol.a);
@@ -491,15 +497,19 @@ void ellipse(float x, float y, float w, float h) {
     }
     glEnd();
     glPopMatrix();
+    glDisable(GL_BLEND);
 }
 
 void rect(float x, float y, float w, float h) {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     y = map(y, 0, height, height, 0);
     h = -h;
     glPushMatrix();
     glTranslated(x, y, 0);
     // Draw rect
-    glColor4d(fillCol.r, fillCol.g, fillCol.b, fillCol.a);
+    glColor4f((float) fillCol.r / (float) 256, (float) fillCol.g / (float) 256, (float) fillCol.b / (float) 256,
+              (float) fillCol.a / (float) 256);
     glBegin(GL_POLYGON);
     glVertex3d(0, 0, 0);
     glVertex3d(0, h, 0);
@@ -507,7 +517,9 @@ void rect(float x, float y, float w, float h) {
     glVertex3d(w, 0, 0);
     glEnd();
     // Draw stroke
-    glColor4d(strokeCol.r, strokeCol.g, strokeCol.b, strokeCol.a);
+    //glColor4d(strokeCol.r, strokeCol.g, strokeCol.b, strokeCol.a);
+    glColor4f((float) strokeCol.r / (float) 256, (float) strokeCol.g / (float) 256, (float) strokeCol.b / (float) 256,
+              (float) strokeCol.a / (float) 256);
     glLineWidth(strokeweight);
     glBegin(GL_LINE_LOOP);
     glVertex3d(0, 0, 0);
@@ -516,10 +528,13 @@ void rect(float x, float y, float w, float h) {
     glVertex3d(w, 0, 0);
     glEnd();
     glPopMatrix();
+    glDisable(GL_BLEND);
 }
 
 void point(float x, float y) {
     y = map(y, 0, height, height, 0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glPushMatrix();
     glTranslated(x, y, 0);
     glColor4d(strokeCol.r, strokeCol.g, strokeCol.b, strokeCol.a);
@@ -531,17 +546,21 @@ void point(float x, float y) {
     }
     glEnd();
     glPopMatrix();
+    glDisable(GL_BLEND);
 }
 
 void line(float x1, float y1, float x2, float y2) {
     y1 = map(y1, 0, height, height, 0);
     y2 = map(y2, 0, height, height, 0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glLineWidth(strokeweight);
     glColor4d(strokeCol.r, strokeCol.g, strokeCol.b, strokeCol.a);
     glBegin(GL_LINES);
     glVertex2d(x1, y1);
     glVertex2d(x2, y2);
     glEnd();
+    glDisable(GL_BLEND);
 }
 
 void strokeWeight(int stroke) {
@@ -565,6 +584,7 @@ void fill(int r, int g = 256, int b = 256, int a = 255) {
 }
 
 void image(PImage img, int x, int y, int w = -1, int h = -1) {
+    y = height - y;
     if (w == -1 && h == -1) {
         w = img.width;
         h = img.height;
@@ -581,16 +601,16 @@ void image(PImage img, int x, int y, int w = -1, int h = -1) {
     glBindTexture(GL_TEXTURE_2D, textureName);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0, 0.0);
-    glVertex2f(x, y);
+    glVertex2f(x, y - h);
 
     glTexCoord2f(1.0, 0.0);
-    glVertex2f(x + w, y);
+    glVertex2f(x + w, y - h);
 
     glTexCoord2f(1.0, 1.0);
-    glVertex2f(x + w, y + h);
+    glVertex2f(x + w, y);
 
     glTexCoord2f(0.0, 1.0);
-    glVertex2f(x, y + h);
+    glVertex2f(x, y);
 
     glEnd();
     glDisable(GL_BLEND);
@@ -636,8 +656,16 @@ void text(char c[], int x, int y) {
 
 }
 
-long millis(){
+long millis() {
     return glutGet(GLUT_ELAPSED_TIME);
+}
+
+void noFill() {
+    fillCol.a = 0;
+}
+
+void noStroke() {
+    strokeCol.a = 0;
 }
 // Math Functions
 
@@ -776,9 +804,134 @@ bool startsWith(const char string[], const char check[]) {
         return true;
     }
 }
+
+float noise(double x) {
+    return noiseCalc(x, 0, octave);
+}
+
+float noise(double x, double y) {
+    return noiseCalc(x, y, octave);
+}
+
+float noise(double x, double y, double z) {
+    return noiseCalc(x, y, octave) * noiseCalc(z, 0, octave);
+}
+
+float noiseCalc(double x, double y, int nbOctave) {
+    int result = 0;
+    int frequence256 = 256;
+    auto sx = (int) ((x) * frequence256);
+    auto sy = (int) ((y) * frequence256);
+    int octave1 = nbOctave;
+    while (octave1 != 0) {
+        int bX = sx & 0xFF;
+        int bY = sy & 0xFF;
+
+        int sxp = sx >> 8;
+        int syp = sy >> 8;
+
+
+        //Compute noise for each corner of current cell
+        int Y1376312589_00 = syp * 1376312589;
+        int Y1376312589_01 = Y1376312589_00 + 1376312589;
+
+        int XY1376312589_00 = sxp + Y1376312589_00;
+        int XY1376312589_10 = XY1376312589_00 + 1;
+        int XY1376312589_01 = sxp + Y1376312589_01;
+        int XY1376312589_11 = XY1376312589_01 + 1;
+
+        int XYBASE_00 = (XY1376312589_00 << 13) ^XY1376312589_00;
+        int XYBASE_10 = (XY1376312589_10 << 13) ^XY1376312589_10;
+        int XYBASE_01 = (XY1376312589_01 << 13) ^XY1376312589_01;
+        int XYBASE_11 = (XY1376312589_11 << 13) ^XY1376312589_11;
+
+        int alt1 = (XYBASE_00 * (XYBASE_00 * XYBASE_00 * 15731 + 789221) + 1376312589);
+        int alt2 = (XYBASE_10 * (XYBASE_10 * XYBASE_10 * 15731 + 789221) + 1376312589);
+        int alt3 = (XYBASE_01 * (XYBASE_01 * XYBASE_01 * 15731 + 789221) + 1376312589);
+        int alt4 = (XYBASE_11 * (XYBASE_11 * XYBASE_11 * 15731 + 789221) + 1376312589);
+
+        /*START BLOCK FOR TRUE GRADIANT NOISE*/
+
+        int grad1X = (alt1 & 0xFF) - 128;
+        int grad1Y = ((alt1 >> 8) & 0xFF) - 128;
+        int grad2X = (alt2 & 0xFF) - 128;
+        int grad2Y = ((alt2 >> 8) & 0xFF) - 128;
+        int grad3X = (alt3 & 0xFF) - 128;
+        int grad3Y = ((alt3 >> 8) & 0xFF) - 128;
+        int grad4X = (alt4 & 0xFF) - 128;
+        int grad4Y = ((alt4 >> 8) & 0xFF) - 128;
+
+
+        int sX1 = bX >> 1;
+        int sY1 = bY >> 1;
+        int sX2 = 128 - sX1;
+        int sY2 = sY1;
+        int sX3 = sX1;
+        int sY3 = 128 - sY1;
+        int sX4 = 128 - sX1;
+        int sY4 = 128 - sY1;
+        alt1 = (grad1X * sX1 + grad1Y * sY1) + 16384 +
+               ((alt1 & 0xFF0000) >> 9); //to avoid seams to be 0 we use an offset
+        alt2 = (grad2X * sX2 + grad2Y * sY2) + 16384 + ((alt2 & 0xFF0000) >> 9);
+        alt3 = (grad3X * sX3 + grad3Y * sY3) + 16384 + ((alt3 & 0xFF0000) >> 9);
+        alt4 = (grad4X * sX4 + grad4Y * sY4) + 16384 + ((alt4 & 0xFF0000) >> 9);
+
+        /*END BLOCK FOR TRUE GRADIANT NOISE */
+
+        /*START BLOCK FOR BICUBIC INTERPOLATION*/
+        int bX2 = (bX * bX) >> 8;
+        int bX3 = (bX2 * bX) >> 8;
+        int _3bX2 = 3 * bX2;
+        int _2bX3 = 2 * bX3;
+        int alt12 = alt1 - (((_3bX2 - _2bX3) * (alt1 - alt2)) >> 8);
+        int alt34 = alt3 - (((_3bX2 - _2bX3) * (alt3 - alt4)) >> 8);
+
+
+        int bY2 = (bY * bY) >> 8;
+        int bY3 = (bY2 * bY) >> 8;
+        int _3bY2 = 3 * bY2;
+        int _2bY3 = 2 * bY3;
+        int val = alt12 - (((_3bY2 - _2bY3) * (alt12 - alt34)) >> 8);
+
+        val *= 256;
+        /*END BLOCK FOR BICUBIC INTERPOLATION*/
+
+
+        //Accumulate in result
+        result += (val << octave1);
+
+        octave1--;
+        sx <<= 1;
+        sy <<= 1;
+    }
+    float tmp = result >> (16 + nbOctave + 1);
+    if (octave == 1) {
+        //29, 96
+        tmp = map(tmp, 29, 96, 0, 1);
+    } else if (octave == 2) {
+        //47, 143
+        tmp = map(tmp, 47, 143, 0, 1);
+    } else if (octave == 3) {
+        //56, 168
+        tmp = map(tmp, 56, 168, 0, 1);
+    } else if (octave == 4) {
+        //60, 178
+        tmp = map(tmp, 60, 178, 0, 1);
+    } else if (octave == 5) {
+        //63, 184
+        tmp = map(tmp, 63, 184, 0, 1);
+    } else if (octave == 6) {
+        //65, 186
+        tmp = map(tmp, 65, 186, 0, 1);
+    } else if (octave == 7) {
+        //-128, 127
+        tmp = map(tmp, -128, 127, 0, 1);
+    }
+    return tmp;
+}
+// Placeholders
 // Classes
 
-// Placeholders
 
 #endif //P5C_H
 #pragma clang diagnostic pop
--- main.cpp
+++ main.cpp
@@ -1,173 +1,33 @@
 #include "P5c.h"
-void KeyPressed();
-void scoreOut();
 
-PVector pos;
-class Player{
-public:
-    int origX;
-    int origY;
-    int x;
-    int y;
-    int w;
-    int h;
-    int score = 0;
-    Player(int x_, int y_, int w_, int h_){
-        x = x_;
-        y = y_;
-        w = w_;
-        h = h_;
-        origX = x;
-        origY = y;
-    }
+using namespace std;
 
-    void draw(){
-        rect(x, y, w, h);
-    }
+float *p;
 
-    void move(int dy){
-        y-=dy;
-    }
-
-    bool bounce(){
-        if(pos.x == x+w){
-            if(pos.y > y && pos.y < y+h) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void addScore(int sc){
-        score += sc;
-    }
-
-    void reset(){
-        x = origX;
-        y = origY;
-    }
-};
-PImage img; //NOLINT
-PVector vel;
-Player *p1 = nullptr;
-Player *p2 = nullptr;
-int state = 0;
-int curtime = 0;
-int counter = 0;
-
-void setup(){
+void setup() {
     size(800, 600);
-    setKeyPressed(KeyPressed);
-    pos.set(width/2, height/2);
-    float rn = random(2);
-    if(rn < 0.5) {
-        vel.set(5, 2);
-    }else if(rn < 1.0){
-        vel.set(5, -2);
-    }else if(rn < 1.5){
-        vel.set(-5, 2);
-    }else if(rn < 2.0){
-        vel.set(-5, -2);
+    p = new float[800];
+    float xOff = 0;
+    float lowest = 255;
+    float highest = -128;
+    for (int x = 0; x < 800; x++) {
+        float n = noise(xOff);
+        n = map(n, 0, 1, 0, height);
+        if(n > highest){
+            highest = n;
+        }
+        if(n < lowest){
+            lowest = n;
+        }
+        *(p + x) = n;
+        xOff+=0.02;
     }
-    p1 = new Player(15, height/2-75, 15, 150);
-    p2 = new Player(width-15, height/2-75, -15, 150);
 }
 
-void draw(){
+void draw() {
     background(51);
-    if(state == 0){
-        text(const_cast<char *>("Press space to start the game"), width / 3, height / 2);
-        return;
-    }
-    if(state == 1){
-        if(curtime == 0){
-            counter = 0;
-            curtime = millis();
-            text("3", width/2, height/2);
-        }
-        if(curtime+1000 <= millis()){
-            curtime = millis();
-            counter++;
-        }
-        if(counter == 3){
-            state = 2;
-            counter = 0;
-        }else{
-            text(const_cast<char *>(std::to_string(3 - counter).c_str()), width / 2, height / 2);
-            return;
-        }
-    }
-    ellipse(pos.x, pos.y, 5, 5);
-    p1->draw();
-    p2->draw();
-    pos.add(vel);
-    if(p2->bounce()){
-        vel.x = -vel.x;
-    }
-    if(p1->bounce()){
-        vel.x = -vel.x;
-    }
-    if(pos.x > width || pos.x < 0){
-        if(pos.x > width){
-            p1->reset();
-            p2->reset();
-            pos.set(width/2, height/2);
-            float rn = random(2);
-            state = 1;
-            if(rn < 0.5) {
-                vel.set(5, 2);
-            }else if(rn < 1.0){
-                vel.set(5, -2);
-            }else if(rn < 1.5){
-                vel.set(-5, 2);
-            }else if(rn < 2.0){
-                vel.set(-5, -2);
-            }
-            p1->addScore(1);
-        }
-        if(pos.x < 0){
-            p1->reset();
-            p2->reset();
-            pos.set(width/2, height/2);
-            float rn = random(2);
-            state = 1;
-            if(rn < 0.5) {
-                vel.set(5, 2);
-            }else if(rn < 1.0){
-                vel.set(5, -2);
-            }else if(rn < 1.5){
-                vel.set(-5, 2);
-            }else if(rn < 2.0){
-                vel.set(-5, -2);
-            }
-            p2->addScore(1);
-        }
-        vel.x = -vel.x;
-    }
-    if(pos.y > height || pos.y < 0){
-        vel.y = -vel.y;
-    }
-    text(const_cast<char *>(std::to_string(p1->score).c_str()), 20, 20);
-    text(const_cast<char *>(std::to_string(p2->score).c_str()), width-20, 20);
-}
-
-void KeyPressed(){
-    if(state == 0){
-        if(key == ' '){
-            state = 1;
-        }
-    }else if(state == 2) {
-        if (key == 'w') {
-            p1->move(15);
-        }
-        if (key == 's') {
-            p1->move(-15);
-        }
-        if (key == 'i') {
-            p2->move(15);
-        }
-        if (key == 'k') {
-            p2->move(-15);
-        }
+    stroke(255);
+    for (int x = 0; x < 800; x++) {
+        point(x, *(p+x));
     }
 }
